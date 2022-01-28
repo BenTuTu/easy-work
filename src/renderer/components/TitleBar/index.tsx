c2966c2f149b5bda6a2f7aadf5b20dfeca99ed88
wangbin <wangbin@iwgame.com>
2022-01-29 02:53:36 +0800

    chore: format code

--- src/renderer/components/TitleBar/index.tsx
+++ src/renderer/components/TitleBar/index.tsx
@@ -6,72 +6,74 @@ import { HorizontalRule, FilterNone, CropSquare, Close } from '@mui/icons-materi
 import s from './index.module.scss';
 
 const defaultTheme = {
-  titleFont: '#afafaf',
-  popperBackground: '#24292f',
+	titleFont: '#afafaf',
+	popperBackground: '#24292f',
 };
 
 const defaultTitleList = [
-  { 
-    title: '工作区(W)',
-    id: 'file',
-    path: '/',
-  }, {
-    title: '历史记录(H)',
-    id: 'history',
-    path: '/build',
-  }
+	{
+		title: '工作区(W)',
+		id: 'file',
+		path: '/',
+	},
+	{
+		title: '历史记录(H)',
+		id: 'history',
+		path: '/build',
+	},
 ];
 
 function TitleBar(props: any) {
-  const { themeColor = defaultTheme, titleList = defaultTitleList } = props;
-  const [winMax, setWinMax] = useState(false);
+	const { themeColor = defaultTheme, titleList = defaultTitleList } = props;
+	const [winMax, setWinMax] = useState(false);
 
-  const minClick = () => {
-    window.ipcRenderer.send("window-min");
-  };
+	const minClick = () => {
+		window.ipcRenderer.send('window-min');
+	};
 
-  const maxClick = () => {
-    setWinMax(!winMax);
-    window.ipcRenderer.send("window-max");
-  };
+	const maxClick = () => {
+		setWinMax(!winMax);
+		window.ipcRenderer.send('window-max');
+	};
 
-  const closeClick = () => {
-    window.ipcRenderer.send("window-close");
-  };
+	const closeClick = () => {
+		window.ipcRenderer.send('window-close');
+	};
 
-  return (
-    <div>
-      <nav className={s.navBar}>
-        <div className={s.navItem}>
-          {titleList.map((item: any) => {
-            return (
-              <div key={item.id} className={s.menuItem}>
-                <Button className={s.menuBtn}>
-                  <Link style={{color: themeColor.titleFont, textDecoration: 'none'}} to={item.path}>{item.title}</Link>
-                </Button>
-              </div>
-            );
-          })}
-        </div>
-        <div>
-            <IconButton className={s.rightBtn} onClick={minClick} component="span">
-              <HorizontalRule className={s.rightIcon}/>
-            </IconButton>
-            <IconButton className={s.rightBtn} onClick={maxClick} component="span">
-              {winMax ? (
-                <FilterNone className={s.rightIcon} />
-              ) : (
-                <CropSquare className={s.rightIcon} />
-              )}
-            </IconButton>
-            <IconButton className={s.closeBtn} onClick={closeClick} component="span">
-              <Close className={s.rightIcon} />
-            </IconButton>
-        </div>
-      </nav>
-      <div className={s.blockFill}></div>
-    </div>
-  );
+	return (
+		<div>
+			<nav className={s.navBar}>
+				<div className={s.navItem}>
+					{titleList.map((item: any) => {
+						return (
+							<div key={item.id} className={s.menuItem}>
+								<Button className={s.menuBtn}>
+									<Link
+										style={{ color: themeColor.titleFont, textDecoration: 'none' }}
+										to={item.path}
+									>
+										{item.title}
+									</Link>
+								</Button>
+							</div>
+						);
+					})}
+				</div>
+				<div>
+					<IconButton className={s.rightBtn} onClick={minClick} component="span">
+						<HorizontalRule className={s.rightIcon} />
+					</IconButton>
+					<IconButton className={s.rightBtn} onClick={maxClick} component="span">
+						{winMax ? <FilterNone className={s.rightIcon} /> : <CropSquare className={s.rightIcon} />}
+					</IconButton>
+					<IconButton className={s.closeBtn} onClick={closeClick} component="span">
+						<Close className={s.rightIcon} />
+					</IconButton>
+				</div>
+			</nav>
+			<div className={s.blockFill}></div>
+		</div>
+	);
 }
 
 export default memo(TitleBar);
--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { MobXProviderContext, observer } from 'mobx-react';
 import ReactDom from 'react-dom';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import TitleBar from '@/renderer/components/TitleBar';
 
 import App from './pages/App';
 import Build from './pages/Build';
@@ -10,11 +11,11 @@ import { Store } from '@/renderer/store';
 
 import './index.scss';
 
-
 const Root = observer(() => {
 	return (
 		<BrowserRouter>
 			<div>
+				<TitleBar />
 				<Switch>
 					<Route exact path="/build">
 						<Build />
